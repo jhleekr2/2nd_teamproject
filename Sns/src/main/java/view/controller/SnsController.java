34fbd861cccb1f0010b11063cc599749a853ce70
user1 <user1@DESKTOP-GP6SU96>
2024-11-06 12:18:44 +0900

    설정 업데이트 11/6 수업 진행상황과 같은 설정으로

--- Sns/src/main/java/view/controller/SnsController.java
+++ Sns/src/main/java/view/controller/SnsController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 @RequestMapping("/sns")
 public class SnsController {
